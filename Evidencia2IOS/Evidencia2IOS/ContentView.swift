6964a479bc2dc8ac17bc18ae513bd37e7c30d3f2
Ali25314 <78836439+Ali25314@users.noreply.github.com>
2021-04-25 02:08:19 -0500

    Add files via upload

---
+++ Evidencia2IOS/Evidencia2IOS/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Evidencia2IOS
+//
+//  Created by user166430 on 4/12/21.
+//  Copyright © 2021 user166430. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hola, Bienvenido Gamer")
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
